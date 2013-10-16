0e64d3f508f3e29818c2811ccaa4420e6e08a573
Kennedy Tran <kennedy.tran@mymail.champlain.edu>
2013-10-16 17:24:36 -0400

    Added a drop down menu for categories. Also added a search bar and a submit button.

---
+++ ChamplainCollegeLibraryApp/gen/edu/champlain/champlaincollegelibraryapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.champlain.champlaincollegelibraryapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
