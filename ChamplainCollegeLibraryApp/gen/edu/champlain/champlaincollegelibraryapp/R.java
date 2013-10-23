52779631a9f25ac84122414cd3e877b1d9d359dd
Kennedy Tran <kennedy.tran@mymail.champlain.edu>
2013-10-23 10:43:22 -0400

    UI fixes to the array and text view.

--- ChamplainCollegeLibraryApp/gen/edu/champlain/champlaincollegelibraryapp/R.java
+++ ChamplainCollegeLibraryApp/gen/edu/champlain/champlaincollegelibraryapp/R.java
@@ -9,7 +9,7 @@ package edu.champlain.champlaincollegelibraryapp;
 
 public final class R {
     public static final class array {
-        public static final int country_arrays=0x7f060000;
+        public static final int entry_array=0x7f060000;
     }
     public static final class attr {
     }
@@ -27,11 +27,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090004;
-        public static final int btnSubmit=0x7f090003;
-        public static final int searchField=0x7f090002;
-        public static final int spinner1=0x7f090001;
-        public static final int spinner2=0x7f090000;
+        public static final int action_settings=0x7f090003;
+        public static final int btnSubmit=0x7f090002;
+        public static final int searchField=0x7f090001;
+        public static final int spinner1=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -42,11 +41,10 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int country_prompt=0x7f050005;
-        public static final int hello_world=0x7f050002;
-        public static final int spinner_title=0x7f050004;
-        public static final int submit=0x7f050006;
-        public static final int title_activity_spinner=0x7f050003;
+        public static final int entry_prompt=0x7f050004;
+        public static final int spinner_title=0x7f050003;
+        public static final int submit=0x7f050005;
+        public static final int title_activity_spinner=0x7f050002;
     }
     public static final class style {
         /** 
--- ChamplainCollegeLibraryApp/src/edu/champlain/champlaincollegelibraryapp/MainActivity.java
+++ ChamplainCollegeLibraryApp/src/edu/champlain/champlaincollegelibraryapp/MainActivity.java
@@ -7,14 +7,16 @@ import java.util.ArrayList;
 import java.util.List;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 public class MainActivity extends Activity {
 
-	private Spinner spinner1, spinner2;
+	private Spinner spinner1;
 	private Button btnSubmit;
 	
     @Override
@@ -22,50 +24,48 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        addItemsOnSpinner2();
         addListenerOnButton();
     	addListenerOnSpinnerItemSelection();
     }
-    
-    public void addItemsOnSpinner2() {
-    	 
-    	spinner2 = (Spinner) findViewById(R.id.spinner2);
-    	List<String> list = new ArrayList<String>();
-    	list.add("list 1");
-    	list.add("list 2");
-    	list.add("list 3");
-    	ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
-    		android.R.layout.simple_spinner_item, list);
-    	dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-    	spinner2.setAdapter(dataAdapter);
-      }
      
-      public void addListenerOnSpinnerItemSelection() {
+    public void addListenerOnSpinnerItemSelection() {
     	spinner1 = (Spinner) findViewById(R.id.spinner1);
     	spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
-      }
+	}
      
       // get the selected dropdown list value
-      public void addListenerOnButton() {
+	public void addListenerOnButton() {
      
     	spinner1 = (Spinner) findViewById(R.id.spinner1);
-    	spinner2 = (Spinner) findViewById(R.id.spinner2);
     	btnSubmit = (Button) findViewById(R.id.btnSubmit);
      
     	btnSubmit.setOnClickListener(new OnClickListener() {
      
-    	  @Override
-    	  public void onClick(View v) {
-     
-    	    Toast.makeText(MainActivity.this,
-    		"OnClickListener : " + 
-                    "\nSpinner 1 : "+ String.valueOf(spinner1.getSelectedItem()) + 
-                    "\nSpinner 2 : "+ String.valueOf(spinner2.getSelectedItem()),
-    			Toast.LENGTH_SHORT).show();
-    	  }
-     
+	    	@Override
+	    	public void onClick(View v) {
+	     
+	    		Toast.makeText(MainActivity.this,
+	    		"OnClickListener : " + 
+	                    "\nSpinner 1 : "+ String.valueOf(spinner1.getSelectedItem()),
+	    			Toast.LENGTH_SHORT).show();
+	    	}
     	});
-      }
+	}
+      
+	public class CustomOnItemSelectedListener implements OnItemSelectedListener {
+		 
+		public void onItemSelected(AdapterView<?> parent, View view, int pos,long id) {
+			Toast.makeText(parent.getContext(), 
+				"OnItemSelectedListener : " + parent.getItemAtPosition(pos).toString(),
+				Toast.LENGTH_SHORT).show();
+		}
+		 
+		@Override
+		public void onNothingSelected(AdapterView<?> arg0) {
+			// TODO Auto-generated method stub
+		}
+		 
+	}  
 
 
     @Override
